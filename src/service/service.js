aa49c7e0113d36061511de4964fc1741f6769e07
Dmitry Mamonov <dmitry.mamonov@corp.mail.ru>
2020-01-27 11:31:13 +0300

    add cli app

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {USER_ARGV_INDEX, DEFAULT_COMMAND, ExitCode} = require(`../constants`);
+const Cli = require(`./cli`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand, userMockItemsCount] = userArguments;
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+if (userMockItemsCount > 1000) {
+  console.info(`Не больше 1000 публикаций`);
+  process.exit(ExitCode.error);
+}
+
+Cli[userCommand].run(userMockItemsCount);
