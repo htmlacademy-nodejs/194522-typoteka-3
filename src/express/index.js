7a35ba943ed77c044dbf2ea603a250945afc1cff
Dmitry Mamonov <indexdm@yandex.ru>
2020-10-19 14:39:20 +0300

    add templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const mainRoutes = require(`./routes/main`);
 const articlesRoutes = require(`./routes/articles`);
 const categoriesRoutes = require(`./routes/categories`);
 const myRoutes = require(`./routes/my`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.resolve(__dirname, `templates`));
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
