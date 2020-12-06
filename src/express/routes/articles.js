bef90753f3f6fb7c6d91834ad696a581df065557
Dmitry Mamonov <indexdm@yandex.ru>
2020-12-07 00:40:35 +0300

    update articles posting

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -1,23 +1,47 @@
 'use strict';
 
 const {Router} = require(`express`);
+const multer = require(`multer`);
+const {nanoid} = require(`nanoid`);
+const path = require(`path`);
 const api = require(`../api`).getAPI();
 
 const articlesRouter = new Router();
+
+const UPLOAD_DIR = `../upload/img/`;
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+
+const upload = multer({storage});
 
 articlesRouter.get(`/add`, async (req, res) => {
   const categories = await api.getCategories();
   res.render(`new-post`, {categories});
 });
 
-articlesRouter.post(`/add`, async (req, res) => {
-  const {body} = req;
+articlesRouter.post(`/add`, upload.single(`cover`), async (req, res) => {
+  const {body, file} = req;
+  const {announce, category, fullText, title, date} = body;
   const data = {
-    announce: body.announce,
-    category: body.category,
-    fullText: body.fullText,
-    title: body.title,
+    announce,
+    category,
+    date,
+    fullText,
+    title,
   };
+
+  if (file) {
+    data.cover = file.filename;
+  }
+
   try {
     await api.postArticle(data);
     res.redirect(`/my`);
