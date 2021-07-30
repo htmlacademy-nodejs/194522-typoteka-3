1abe28c9ee140cde69a2d71d93740873aafa9468
dm-gthb <indexdm@yandex.ru>
2021-07-30 19:49:45 +0300

    naming fix

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE categories(
 
 CREATE TABLE users(
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  firstname varchar(250) NOT NULL,
-  lastname varchar(250) NOT NULL,
+  first_name varchar(250) NOT NULL,
+  last_name varchar(250) NOT NULL,
   email varchar(250) NOT NULL UNIQUE,
   password_hash varchar(250) NOT NULL,
   avatar varchar(250) NOT NULL
@@ -16,7 +16,7 @@ CREATE TABLE articles(
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   title varchar(250) NOT NULL,
   announce varchar(250) NOT NULL,
-  createdAt timestamp DEFAULT current_timestamp,
+  created_at timestamp DEFAULT current_timestamp,
   text text,
   image varchar(250),
   user_id integer NOT NULL,
@@ -24,17 +24,17 @@ CREATE TABLE articles(
 );
 
 CREATE TABLE articles_categories(
-  articles_id integer NOT NULL,
-  categories_id integer NOT NULL,
-  FOREIGN KEY (articles_id) REFERENCES articles(id),
-  FOREIGN KEY (categories_id) REFERENCES categories(id),
-  PRIMARY KEY (articles_id, categories_id)
+  article_id integer NOT NULL,
+  category_id integer NOT NULL,
+  FOREIGN KEY (article_id) REFERENCES articles(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  PRIMARY KEY (article_id, category_id)
 );
 
 CREATE TABLE comments(
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   text text NOT NULL,
-  createdAt timestamp DEFAULT current_timestamp,
+  created_at timestamp DEFAULT current_timestamp,
   user_id integer NOT NULL,
   article_id integer NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
